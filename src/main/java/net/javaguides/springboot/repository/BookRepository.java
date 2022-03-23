08bd36b52f68c4463db7aed07911b1bc57c86d77
vivekamiq <101623886+vivekamiq@users.noreply.github.com>
2022-03-24 00:03:31 +0530

    Delete src directory

--- src/main/java/net/javaguides/springboot/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.springboot.repository;
-
-import net.javaguides.springboot.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BookRepository extends JpaRepository<Book,Long> {
-
-}
